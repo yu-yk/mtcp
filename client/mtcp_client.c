fbeb89c4bea9fbd6e2c9c0af83a5a80a9e419519
YU Yuk Kuen <tgp146871@gmail.com>
2017-03-14 16:18:36 +0800

    first compile of client
    first compile of client

--- client/mtcp_client.c
+++ client/mtcp_client.c
@@ -20,7 +20,7 @@ typedef struct mtcpheaders
 struct arg_list
 {
 	int socket;
-	struct sockaddr_in *server_addr; 
+	struct sockaddr_in *server_addr;
 };
 
 /* ThreadID for Sending Thread and Receiving Thread */
@@ -34,6 +34,10 @@ static pthread_cond_t send_thread_sig = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t send_thread_sig_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static pthread_mutex_t info_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/* The Sending Thread and Receive Thread Function */
+static void *send_thread();
+static void *receive_thread();
 
 /* Connect Function Call (mtcp Version) */
 void mtcp_connect(int socket_fd, struct sockaddr_in *server_addr){
@@ -52,9 +56,9 @@ void mtcp_connect(int socket_fd, struct sockaddr_in *server_addr){
 
 	// then waiting
 	pthread_mutex_lock(&app_thread_sig_mutex);
-	while(!app_thread_sig) {
+	// while(!app_thread_sig) {
 		pthread_cond_wait(&app_thread_sig, &app_thread_sig_mutex); // wait
-	}
+	// }
 	pthread_mutex_unlock(&app_thread_sig_mutex);
 
 	return;
@@ -63,6 +67,7 @@ void mtcp_connect(int socket_fd, struct sockaddr_in *server_addr){
 /* Write Function Call (mtcp Version) */
 int mtcp_write(int socket_fd, unsigned char *buf, int buf_len){
 
+	return 0;
 }
 
 /* Close Function Call (mtcp Version) */
@@ -71,44 +76,44 @@ void mtcp_close(int socket_fd){
 }
 
 static void *send_thread(void *server_arg){
-
+	struct arg_list *arg = (struct arg_list *)server_arg;
 	/*************************************************************************
 	 *********************** Three Way Handshake *****************************
 	 *************************************************************************/
 
-	// waiting 
+	// waiting
 	pthread_mutex_lock(&send_thread_sig_mutex);
-	while(!send_thread_sig) {
+	// while(!send_thread_sig) {
 		pthread_cond_wait(&send_thread_sig, &send_thread_sig_mutex); // wait
-	}
+	// }
 	pthread_mutex_unlock(&send_thread_sig_mutex);
 
 	// construct mtcp header
 	mtcpheader SYN;
 	SYN.seq = 0;
 	SYN.mode = '0';
-	memcpy(SYN.buffer, SYN.&seq, 4);
+	memcpy(SYN.buffer, &SYN.seq, 4);
 	SYN.buffer[0] = SYN.buffer[0] | (SYN.mode << 4);
 
 	// send SYN to server
-	sendto(server_arg.socket_fd, SYN.buffer, strlen(SYN.buffer)+1, 0, server_arg.server_addr, sizeof(server_arg.server_addr));
+	sendto(arg->socket, SYN.buffer, strlen(SYN.buffer)+1, 0, arg->server_addr, sizeof(arg->server_addr));
 
 	// waiting again
 	pthread_mutex_lock(&send_thread_sig_mutex);
-	while(!send_thread_sig) {
+	// while(!send_thread_sig) {
 		pthread_cond_wait(&send_thread_sig, &send_thread_sig_mutex); // wait
-	}
+	// }
 	pthread_mutex_unlock(&send_thread_sig_mutex);
 
 	// send ACK to server
 	mtcpheader ACK;
 	ACK.seq = 1;
 	ACK.mode = '4';
-	memcpy(ACK.buffer, ACK.&seq, 4);
+	memcpy(ACK.buffer, &ACK.seq, 4);
 	ACK.buffer[0] = ACK.buffer[0] | (ACK.mode << 4);
 
 	// send ACK to server
-	sendto(server_arg.socket_fd, ACK.buffer, strlen(ACK.buffer)+1, 0, server_arg.server_addr, sizeof(server_arg.server_addr));
+	sendto(arg->socket, ACK.buffer, strlen(ACK.buffer)+1, 0, arg->server_addr, sizeof(arg->server_addr));
 
 	// wake up main thread
 	pthread_cond_signal(&app_thread_sig);
@@ -120,23 +125,24 @@ static void *send_thread(void *server_arg){
 
 static void *receive_thread(void *server_arg){
 	unsigned char buf[4];
+	struct arg_list *arg = (struct arg_list *)server_arg;
 
 	// monitor for the SYN-ACK
-	if(recvfrom(server_arg.socket_fd, buf, sizeof(buf), 0, server_arg.server_addr, sizeof(server_arg.server_addr)) < 0)
+	if(recvfrom(arg->socket, buf, sizeof(buf), 0, arg->server_addr, sizeof(arg->server_addr)) < 0)
 	{
 		printf("receive error\n");
 		exit(1);
-	} 
-	
+	}
+
 	// keep monitoring
 	while (1) {
 		// decode header
 		mtcpheader header;
 		header.mode = buf[0] >> 4;
 		header.buffer[0] = buf[0] & 0x0F;
-		memcpy(header.&seq, header.buffer, 4);
+		memcpy(&header.seq, header.buffer, 4);
 		header.seq = ntohl(header.seq);
-		switch(mode) {
+		switch(header.mode) {
 			case '1': // SYN-ACK
 				// when SYN-ACK received
 				pthread_cond_signal(&send_thread_sig);
